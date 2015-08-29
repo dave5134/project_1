7f43db2a785ef883669523be484497a167f28488
Devendra Prasai <daveprasai@gmail.com>
2015-08-29 00:33:40 -0400

    project_1
    My first project on git

---
+++ js/images.js
@@ -0,0 +1,20 @@
+          function displayNextImage() {
+              x = (x === images.length - 1) ? 0 : x + 1;
+              document.getElementById("img").src = images[x];
+          }
+
+          function displayPreviousImage() {
+              x = (x <= 0) ? images.length - 1 : x - 1;
+              document.getElementById("img").src = images[x];
+          }
+
+          function startTimer() {
+              setInterval(displayNextImage, 3000);
+          }
+
+          var images = [], x = -1;
+          images[0] = "images/pizza1.jpg";
+          images[1] = "images/pizza2.jpg";
+          images[2] = "images/pizza3.jpg";
+          images[3] = "images/pizza4.jpg";
+    
